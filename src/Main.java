adcd020ff1d77ae3ae30644a5aa03749568bbdaa
maryam mrg <maryamelouadaa@gmail.com>
2024-12-20 17:01:06 +0100

    menu of the app

--- src/Main.java
+++ src/Main.java
@@ -9,12 +9,15 @@ import java.util.Scanner;
 
 
         public static void displayAllBooks() {
-            for (int i = 0; i < books.size(); i++) {
+            if(books.size()==0){
+                System.out.println("no books yet to display");
+            }else {
+                for (int i = 0; i < books.size(); i++) {
 
-                books.get(i).toString();
+                    books.get(i).toString();
+                }
             }
         }
-
         public  static void  addBook (){
 
             Book newBook = new Book("", "", "");
@@ -30,7 +33,6 @@ import java.util.Scanner;
 
             books.add(newBook);
         }
-
         public static void searchBook() {
 
         }
@@ -38,9 +40,53 @@ import java.util.Scanner;
 
 
         }
+        public static void updateBook() {
+
+        }
+
 
 
         public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
+            int choice=0;
+            while (choice!=4){
+                System.out.println("====System management library====");
+                System.out.println("======         MENU        ======");
+                System.out.println("1.========  Add a book =========");
+                System.out.println("2.======  Display all books ====");
+                System.out.println("3.=======  Search a book =======");
+                System.out.println("4.=======  Update a book =======");
+                System.out.println("5.=======  Remove a book =======");
+                System.out.println("6.============= Exit ===========");
+                System.out.println("enter your choice please: ");
+                choice = scanner.nextInt();
+                if(choice==6){
+                    System.out.println("========== by by (^-^)==========");
+                    break;
+                }
+                switch (choice){
+                    case 1:
+                        addBook();
+                        break;
+                    case 2:
+                        displayAllBooks();
+                        break;
+
+                    case 3:
+                        searchBook();
+                        break;
+                     case 4:
+                         updateBook();
+                         break;
+                    case 5:
+                        removeBook();
+                        break;
+
+                    default:
+                        System.out.println("invalid choice");
+
+                }
+            }
 
         }
     }
